ec8f8e777e5288e653fc44fe50d46f530fbebf70
AsaduzzamanSam <samrat.asad@gmail.com>
2021-11-23 18:13:02 +0600

    set name_route navigator for those screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_with_firebase/screens/login_screen.dart';
+import 'package:flutter_chat_app_with_firebase/screens/chat_screen.dart';
+import 'package:flutter_chat_app_with_firebase/screens/registration_screen.dart';
 import 'package:flutter_chat_app_with_firebase/screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
@@ -8,12 +11,19 @@ class FlashChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/chatScreen': (context) => ChatScreen(),
+        '/loginScreen': (context) => LoginScreen(),
+        '/registrationScreen': (context) => RegistrationScreen(),
+      },
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+
     );
   }
 }
